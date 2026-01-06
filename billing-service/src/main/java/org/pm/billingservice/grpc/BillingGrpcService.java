f7ac5802b9285fbb7aa82786b46b2c37a37b2288
anuragrajput <arajput3000@gmail.com>
2026-01-06 19:20:22 +0530

    billing service and protobuf

---
+++ billing-service/src/main/java/org/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package org.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+        StreamObserver<BillingResponse> responseObserver){
+
+        log.info("createBillingAccount request recieved {}" , billingRequest.toString());
+
+        //billing service business logic
+
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("randomAccountId123123123")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
+}
